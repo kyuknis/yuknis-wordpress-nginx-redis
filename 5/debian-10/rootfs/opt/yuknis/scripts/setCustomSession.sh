3692b8bfc656bb84a828faaf4bd0a58ef8aa2b4c
Kirkland <kirkland@yuknis.com>
2022-01-05 21:03:57 -0500

    bug fixes

--- 5/debian-10/rootfs/opt/yuknis/scripts/setCustomSession.sh
+++ 5/debian-10/rootfs/opt/yuknis/scripts/setCustomSession.sh
@@ -15,4 +15,4 @@ echo Setting PHP Session variables...
 sed -i "s|session.save_handler = files|session.save_handler = $PHP_SESSION_SAVE_HANDLER|g" /opt/bitnami/php/etc/php.ini
 sed -i "s|session.save_path = /opt/bitnami/php/var/run/session|session.save_path = $PHP_SESSION_SAVE_PATH|g" /opt/bitnami/php/etc/php.ini
 
-/opt/yuknis/scripts/defaultBehavior.sh $1
+exec /opt/yuknis/scripts/defaultBehavior.sh "$@"
